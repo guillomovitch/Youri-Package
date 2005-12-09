f9095d7c5da0be58796a77ec1aaa0a77d058997f
Guillaume Rousse <guillomovitch@gmail.com>
2005-12-09 16:23:58 +0000

    new files and dependencies accessors

--- lib/Youri/Package/Base.pm
+++ lib/Youri/Package/Base.pm
@@ -16,6 +16,12 @@ use Carp;
 use strict;
 use warnings;
 
+use constant DEPENDENCY_NAME => 0;
+use constant DEPENDENCY_RANGE => 1;
+
+use constant FILE_NAME => 0;
+use constant FILE_MODE => 1;
+use constant FILE_MD5SUM => 2;
 
 =head2 new()
 
