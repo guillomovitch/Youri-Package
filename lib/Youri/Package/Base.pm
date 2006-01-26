6c573e9023fa06d18c2c8f8fd72bfb0f81cfad2b
Guillaume Rousse <guillomovitch@gmail.com>
2006-01-26 21:30:37 +0000

    keep authors in a centralized place add copyright statement in each file

--- lib/Youri/Package/Base.pm
+++ lib/Youri/Package/Base.pm
@@ -172,6 +172,12 @@ sub gpg_key {
 
 All instances methods have to be implemented.
 
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2002-2006, YOURI project
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
 =cut
 
 1;
