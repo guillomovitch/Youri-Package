41e49faa8597d4a7bde845b8fe6b4732f7256e89
Guillaume Rousse <guillomovitch@gmail.com>
2011-01-22 10:21:48 +0000

    adaptation to new rpm5-syle filename (Per Oyvind Karlsen)

--- lib/Youri/Package/RPM/Test.pm
+++ lib/Youri/Package/RPM/Test.pm
@@ -18,6 +18,7 @@ use File::Basename;
 use Carp;
 use URPM;
 use base 'Youri::Package::RPM';
+use feature qw(switch);
 use overload
     '""'     => 'as_string',
     '0+'     => '_to_number',
@@ -110,13 +111,27 @@ sub _init {
         croak "non-readable file $options{file}"
             unless -r $options{file};
         my $filename = basename($options{file});
-        croak "non-compliant filename $filename"
-            unless $filename =~/^([\w-]+)-([^-]+)-([^-]+)\.(\w+)\.rpm$/;
-        $self->{_tags}->{name} = $1;
-        $self->{_tags}->{version} = $2;
-        $self->{_tags}->{release} = $3;
-        $self->{_tags}->{arch} = $4;
-        $self->{_file} = $options{file};
+        given ($filename) {
+            when (/^([\w-]+)-([^-]+)-([^-]+)\.(\w+)\.rpm$/) {
+                # rpm4 style package, with combined dist suffix and release
+                $self->{_tags}->{name} = $1;
+                $self->{_tags}->{version} = $2;
+                $self->{_tags}->{release} = $3;
+                $self->{_tags}->{arch} = $4;
+                $self->{_file} = $options{file};
+            }
+            when (/^([\w-]+)-([^-]+)-([^-]+)-([^-]+)\.(\w+)\.rpm$/) {
+                # rpm5 style package, with distinct dist suffix and release
+                $self->{_tags}->{name} = $1;
+                $self->{_tags}->{version} = $2;
+                $self->{_tags}->{release} = $3;
+                $self->{_tags}->{arch} = $5;
+                $self->{_file} = $options{file};
+            }
+            default {
+                croak "non-compliant filename $filename";
+            }
+        }
     }
 
     $self->{_requires}  = $options{requires};
